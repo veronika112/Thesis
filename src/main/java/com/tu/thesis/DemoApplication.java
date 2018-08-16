fb627117a69058e044b75aa1133d0cb26a174217
Veronika <veronika.vakadinova@abv.bg>
2018-08-16 22:00:23 +0300

    initial commit

---
+++ src/main/java/com/tu/thesis/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.tu.thesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
